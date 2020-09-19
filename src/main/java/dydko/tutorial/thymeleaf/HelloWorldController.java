88484bc71d07ccbca04da565f403cb047aa1f718
dydko1 <miroslaw.dyduch@gmail.com>
2020-09-19 07:30:37 +0200

    lessson 01

--- src/main/java/dydko/tutorial/thymeleaf/HelloWorldController.java
+++ src/main/java/dydko/tutorial/thymeleaf/HelloWorldController.java
@@ -1,11 +1,15 @@
 package dydko.tutorial.thymeleaf;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
 
-    public String hello() {
-
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message","Hello World");
+        return "helloworld";
     }
 }
