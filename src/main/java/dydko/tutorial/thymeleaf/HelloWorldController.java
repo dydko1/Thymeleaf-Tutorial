9f205ce1a0a7bedea767da9ede73b0dbe8a6e4d7
dydko1 <miroslaw.dyduch@gmail.com>
2020-09-19 07:02:52 +0200

    initial commit

---
+++ src/main/java/dydko/tutorial/thymeleaf/HelloWorldController.java
@@ -0,0 +1,11 @@
+package dydko.tutorial.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController {
+
+    public String hello() {
+
+    }
+}
